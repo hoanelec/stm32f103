aaf48c6edc0be0ee3997acdf7857b0addac0bd65
hoanelec <danghoanelec@gmail.com>
2021-03-26 17:04:09 +0700

    Ir_receiver

--- Project/kit/3_Infrared_Sensor_Modules/Core/Src/main.c
+++ Project/kit/3_Infrared_Sensor_Modules/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+//#include "stm32f1xx_hal_tim.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,20 +40,110 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+uint8_t buff[10];
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
+TIM_HandleTypeDef htim1;
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define IR_in GPIO_PIN_0
+#define IR_out GPIO_PIN_1
+TIM_HandleTypeDef htimer;
+void Delay_us(uint16_t time) {
+	/* change your code here for the Delay_us in microseconds */
+	__HAL_TIM_SET_COUNTER(&htimer, 0);  // set the counter value a 0
+	while ((__HAL_TIM_GET_COUNTER(&htimer)) < time)
+		;  // wait for the counter to reach the us input in the parameter
+}
 
+void Timer_1us_Init(uint8_t _freq, TIM_HandleTypeDef _htim,TIM_TypeDef* _Instance) {
+	TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+	TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+	_htim.Instance = _Instance;
+	_htim.Init.Prescaler = _freq - 1;
+	_htim.Init.CounterMode = TIM_COUNTERMODE_UP;
+	_htim.Init.Period = 0xffff - 1;
+	_htim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	_htim.Init.RepetitionCounter = 0;
+	_htim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&_htim) != HAL_OK) {
+		Error_Handler();
+	}
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(&_htim, &sClockSourceConfig) != HAL_OK) {
+		Error_Handler();
+	}
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(&_htim, &sMasterConfig)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	htimer = _htim;
+	HAL_TIM_Base_Start(&_htim);
+}
+uint32_t IrRead()
+{
+	uint32_t code=0;
+	if(HAL_GPIO_ReadPin(GPIOB, IR_in) )
+	{
+		HAL_GPIO_WritePin(GPIOB, IR_out, GPIO_PIN_SET);
+
+		__HAL_TIM_SET_COUNTER(&htimer, 0);  // set the counter value a 0
+		while(HAL_GPIO_ReadPin(GPIOB, IR_in));//9us
+
+		HAL_GPIO_WritePin(GPIOB, IR_out, GPIO_PIN_RESET);
+
+		if((uint16_t)__HAL_TIM_GET_COUNTER(&htimer)>7000&&(uint16_t)__HAL_TIM_GET_COUNTER(&htimer)<10000)
+		{
+			__HAL_TIM_SET_COUNTER(&htimer, 0);  // set the counter value a 0
+			while(!HAL_GPIO_ReadPin(GPIOB, IR_in));//4.5ms
+
+			HAL_GPIO_WritePin(GPIOB, IR_out, GPIO_PIN_SET);
+
+			if((uint16_t)__HAL_TIM_GET_COUNTER(&htimer)>4000&&(uint16_t)__HAL_TIM_GET_COUNTER(&htimer)<6000)
+			{
+							for (int i=0; i<32; i++)
+								  {
+									__HAL_TIM_SET_COUNTER(&htimer, 0);  // set the counter value a 0
+
+									  while (!(HAL_GPIO_ReadPin (GPIOB, IR_in))); // wait for pin to go high.. this is 562.5us LOW
+										HAL_GPIO_WritePin(GPIOB, IR_out, GPIO_PIN_SET);
+									  while ((HAL_GPIO_ReadPin (GPIOB, IR_in)));  // count the space length while the pin is high
+										HAL_GPIO_WritePin(GPIOB, IR_out, GPIO_PIN_RESET);
+									  if ((uint16_t)__HAL_TIM_GET_COUNTER(&htimer) > 1200) // if the space is more than 1.2 ms
+									  {
+										  code |= (1UL << (31-i));   // write 1
+									  }
+
+									  else code &= ~(1UL << (31-i));  // write 0
+								  }
+							return code;
+			}
+
+//			if(__HAL_TIM_GET_COUNTER(&htimer)>4000&&__HAL_TIM_GET_COUNTER(&htimer)<5000)
+//			{
+//
+//			}
+
+		}
+	}
+	return code;
+}
 /* USER CODE END 0 */
 
 /**
@@ -83,17 +173,31 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  Timer_1us_Init(72,htim1,TIM1);
+ // HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, 0);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  uint32_t Ir_recv = IrRead();
+	  HAL_Delay(1);
+	//  sprintf(buff,"0x%08 PRIx32 ",Ir_recv);
+	//  HAL_UART_Transmit(&huart1, &buff, sprintf(buff,"0x%08 PRIx32 ",Ir_recv), 500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+//	  for(int i = 0; i<50000;i++)
+//	  {
+//		  Delay_us(20);
+//	  }
+	//  HAL_GPIO_TogglePin(GPIOB, IR_out);
+	//  HAL_Delay(1);
   }
   /* USER CODE END 3 */
 }
@@ -110,10 +214,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -122,15 +229,137 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
