ce58aca8f25ae4eb44e77d9fc2102fd17f7a0056
hoanelec <danghoanelec@gmail.com>
2021-04-09 17:10:23 +0700

    ?

---
+++ Project/sensors/2_Force_Sensing_Register/Core/Src/Uart.c
@@ -0,0 +1,23 @@
+#include "Uart.h"
+void SendUnsignedNumber(uint32_t number,UART_HandleTypeDef _huart)
+{
+	uint8_t l = sprintf(uartBuffer,"%lu",number);
+	HAL_UART_Transmit(&huart1, (uint8_t *)uartBuffer, l, 500);
+}
+void SendConstChar(const char* constChar,UART_HandleTypeDef _huart)
+{
+	HAL_UART_Transmit(&_huart,(uint8_t *)constChar,strlen((const char*)constChar),200);
+}
+void SendFloat(float num,UART_HandleTypeDef _huart)
+{
+	uint16_t intPart = (uint16_t)num;
+	float  decPart  = (float )num - intPart;
+	uint16_t decPart2 = (decPart*100)/10;
+	sprintf(uartBuffer,"%d.%d",intPart,decPart2);
+	HAL_UART_Transmit(&_huart, (uint8_t *)uartBuffer, sprintf(uartBuffer,"%d.%d",intPart,decPart2), 400);
+}
+void SendFloatLn(float num,UART_HandleTypeDef _huart)
+{
+	SendFloat(num,_huart);
+	HAL_UART_Transmit(&_huart, (uint8_t *)"\n", 2, 50);
+}
